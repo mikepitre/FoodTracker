c7d69793345097836861075e2224fb4488744cf1
mikepitre <michaelepitre@gmail.com>
2015-09-30 08:57:30 -0500

    added ratingControl UI subview

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,15 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Mike Pitre on 9/30/15.
+//  Copyright © 2015 Mike Pitre. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+   
+
+}
